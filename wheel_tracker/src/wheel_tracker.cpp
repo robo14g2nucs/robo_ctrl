1ecddc282996da82ccdef183d6d4802b65d7e8fa
mgeppert <geppert@kth.se>
2014-11-26 22:33:30 +0100

    added filter to ir sensor values

--- wheel_tracker/src/wheel_tracker.cpp
+++ wheel_tracker/src/wheel_tracker.cpp
@@ -14,8 +14,8 @@ private:
 	double curX, curY;
 	double denc1, denc2;
 	const double DEGTOM; 
-    double leftIrRelAngle;
-    double rightIrRelAngle;
+//    double leftIrRelAngle;
+//    double rightIrRelAngle;
 
 public:
 
@@ -23,8 +23,15 @@ public:
 	ros::Subscriber encoders_subscriber_;
     ros::Subscriber irSub;
 	ros::Publisher posori_publisher_;
+    double alphama; // weight for exponential weighted moving average filter
+    double Ffront_left;
+    double Ffront_right;
+    double Fback_left;
+    double Fback_right;
+    bool Fboolean;
 
-	wheel_tracker_node() : curAngle(0.0), curX(1.4), curY(2.25), DEGTOM( WHEEL_RADIUS*TWOPI/TICKSPR )
+
+    wheel_tracker_node() : curAngle(0.0), curX(1.4), curY(2.25), DEGTOM( WHEEL_RADIUS*TWOPI/TICKSPR ), alphama(0.3), Fboolean(0)
 	{
 		n_ = ros::NodeHandle("~");
 		encoders_subscriber_ = n_.subscribe("/arduino/encoders", 1, &wheel_tracker_node::update, this);
@@ -68,17 +75,46 @@ public:
 
     void irCallback(const ir_reader::distance_readings::ConstPtr &msg){
 
+        double leftIrRelAngle = 100;
+        double rightIrRelAngle = 100;
+
+        if (!Fboolean){
+            Ffront_left = msg->front_left;
+            Ffront_right = msg->front_right;
+            Fback_left = msg->back_left;
+            Fback_right = msg->back_right;
+            Fboolean = 1;
+        }
+
+        Ffront_left = Ffront_left * (1-alphama) + msg->front_left * (alphama);
+        Ffront_right = Ffront_right * (1-alphama) + msg->front_right * (alphama);
+        Fback_left = Fback_left * (1-alphama) + msg->back_left * (alphama);
+        Fback_right = Fback_right * (1-alphama) + msg->back_right * (alphama);
+
         if(msg->front_left < 25 && msg->back_left < 25){
             //compute angle to left wall
             double diff = msg->front_left - msg->back_left;
-            leftIrRelAngle = std::atan2(diff, IR_SIDE_SENSOR_DISTANCE*100);
+            leftIrRelAngle = M_PI_2 - std::atan2(diff, IR_SIDE_SENSOR_DISTANCE*100);
         }
         if(msg->front_right < 25 && msg->back_right < 25){
             double diff = msg->front_right - msg->back_right;
             rightIrRelAngle = std::atan2(diff, IR_SIDE_SENSOR_DISTANCE*100);
         }
 
+        ROS_ERROR("old odomtry angle: %f", curAngle);
+        double odomRelAngle = fmod(curAngle, M_PI_2);
+        double offset = curAngle - odomRelAngle;
+        ROS_ERROR("relative odometry: %f", odomRelAngle);
+        if(fabs(odomRelAngle - leftIrRelAngle) < 0.2 && fabs(odomRelAngle - leftIrRelAngle) < fabs(odomRelAngle - rightIrRelAngle)){
+            curAngle = offset + (9*odomRelAngle + leftIrRelAngle) / 10.0d;
+        }
+        else if(fabs(odomRelAngle - rightIrRelAngle) < 0.2 && fabs(odomRelAngle - rightIrRelAngle) < fabs(odomRelAngle - leftIrRelAngle)){
+            curAngle = offset + (9*odomRelAngle + rightIrRelAngle) / 10.0d;
+        }
+
         ROS_ERROR("angle from IR: left: %f, right: %f", leftIrRelAngle, rightIrRelAngle);
+        ROS_ERROR("new odometry angle: %f", curAngle);
+
     }
 
 };
